075ca7e23ccef74fd1b33ac7bc2fcd5956550e18
Ion G <98644414+Johny36@users.noreply.github.com>
2022-04-26 12:49:45 +0300

    Update Cycle-for-PARKING_PLACES.py

--- Cycle-for-PARKING_PLACES.py
+++ Cycle-for-PARKING_PLACES.py
@@ -11,3 +11,5 @@ for place_index in range(1, PARKING_PLACES):
     
 
 print("\n","#"*PARKING_PLACES*5, sep="")
+
+## sep - inseamna separator. Se foloseste pentru introducerea oarecarui simbol intre valorile introduse, sau ca in cazul nostru sa nu fie delimitat prin spatiu.
